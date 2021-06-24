4c600444a0700c72c4f68467ebd16bc2ff6c9615
Nicko141 <60990418+Nicko141@users.noreply.github.com>
2021-06-24 16:42:31 +1000

    menus done

---
+++ Assets/Scripts/SceneManagement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour
+{
+    public void LoadScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        Application.Quit();
+    }
+}
